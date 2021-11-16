5f68ec70fdfc314a85fbbd6ff603377fbc4cc2ed
Jason Law <jason.cclaw@gmail.com>
2021-11-16 11:34:49 +0800

    remove language and dart clean up

--- lib/src/GoogleMLKit.dart
+++ lib/src/GoogleMLKit.dart
@@ -1,3 +1,3 @@
 export 'google_ml_kit.dart';
 export 'vision/ml_vision.dart';
-export 'nlp/NLP.dart';
+//export 'nlp/NLP.dart';
--- lib/src/google_ml_kit.dart
+++ lib/src/google_ml_kit.dart
@@ -1,9 +1,9 @@
 import 'vision/ml_vision.dart';
-import 'nlp/NLP.dart';
+// import 'nlp/NLP.dart';
 
 class GoogleMlKit {
   GoogleMlKit._();
 
   static final Vision vision = Vision.instance;
-  static final NaturalLanguage nlp = NaturalLanguage.instance;
+  //static final NaturalLanguage nlp = NaturalLanguage.instance;
 }
